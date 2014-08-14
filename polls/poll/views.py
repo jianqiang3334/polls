b3d18f3b8118ed59f1781ae48c3f620286073ca1
jianqiang3334 <15281063334@163.com>
2014-08-14 21:16:46 +0800

    simple poll web

--- polls/poll/views.py
+++ polls/poll/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from poll.models import Poll,Choice
+from django.shortcuts import render_to_response,HttpResponseRedirect,HttpResponse
 # Create your views here.
+def pollWeb(request):
+    pollObj = Poll.objects.all()
+    choiceObj = Choice.objects.all()
+    return render_to_response('pollWeb.html', {'Poll': pollObj,'Choice':choiceObj})
+def submit(request):
+    choiceObj = Choice.objects.all()
+    h=0
+    html="html"
+    for choice in choiceObj:
+        #htm=choice.poll_id
+        choice_key = "choice_"+str(choice.poll_id)
+        if choice_key in request.GET:
+            html=html+str(request.GET[choice_key])
+            if choice.id == int(request.GET[choice_key]):
+                choice.votes += 1
+                choice.save()
+    return HttpResponseRedirect("/pollWeb/")
+
+
+
+
+
+
+
--- polls/polls/urls.py
+++ polls/polls/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
+from poll.views import pollWeb,submit
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +10,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^pollWeb/$',pollWeb),
+    url(r'^submit/$',submit),
 )
